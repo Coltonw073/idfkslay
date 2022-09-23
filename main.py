8ddc84c3b21fa26d4402d0df2a3a6f896482228b
Coltonw073 <51644059+Coltonw073@users.noreply.github.com>
2022-09-23 10:43:35 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+from flask import request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+cors = CORS(app)
+@app.route("/get", methods=["GET"])
+def postME():
+    return requests.get(request.headers['URL']).text
+app.run(debug=False)
